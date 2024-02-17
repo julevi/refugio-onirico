1f22084fd300a4b6ff5b730eb989b610b860b36e
Juliana <jprado.contact@gmail.com>
2024-02-17 19:17:28 -0300

    Secoes da pagina de home completas

--- src/components/Apresentacao.jsx
+++ src/components/Apresentacao.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import video from '../assets/img/video.png';
 
+
 const Apresentacao = () => {
   return (
     <div className="bg-cinza">
     <div className="container max-w-7xl mx-auto px-10">
         <div className="flex flex-col lg:flex-row items-center justify-center py-20 ">
           <div className="text-center lg:text-left lg:mr-36">
-            <h1 className="leading-tight text-7xl font-bold text-white mb-4">Ensino de animação com processos fáceis e intuitivos.</h1>
-            <h1 className="text-7xl font-bold text-amarelo mb-4">Vamos Animar!</h1>
+            <h1 className="text-4xl leading-tight lg:text-7xl font-bold text-white mb-4">Ensino de animação com processos fáceis e intuitivos.</h1>
+            <h1 className="text-4xl lg:text-7xl font-bold text-amarelo mb-4">Vamos Animar!</h1>
           </div>
           <img src={video} className="mt-10 lg:mt-0" alt="" />
         </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,22 @@
 import React from 'react';
 import Header from '../components/Header';
 import Apresentacao from '../components/Apresentacao';
+import RefugioCursos from '../components/RefugioCursos';
+import EncontreAqui from '../components/EncontreAqui';
+import AcesseAqui from '../components/AcesseAqui';
+import Professor from '../components/Professor';
+import Footer from '../components/Footer';
 
 function Home() {
   return (
     <>
-    
       <Header />
       <Apresentacao />
+      <RefugioCursos/>
+      <EncontreAqui/>
+      <AcesseAqui/>
+      <Professor/>
+      <Footer/>
     </>
   );
 }
