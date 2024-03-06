29844ea3e9ee1ec15e410e16ffce01c1b398b49c
Juliana <jprado.contact@gmail.com>
2024-03-06 09:42:39 -0300

    Configuração do react dom router

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { createServer } from 'vite';
+import historyApiFallback from 'connect-history-api-fallback';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: "/refugio-onirico",
-})
+  server: {
+    middleware: [historyApiFallback()]
+  }
+});
