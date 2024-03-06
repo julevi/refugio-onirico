1bbc4bfd08fe6648bf0bf34428d52f8c0c277951
Juliana <jprado.contact@gmail.com>
2024-03-06 09:25:07 -0300

    Quadro de cursos completo

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
-import { useState } from 'react'
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import Home from './pages/Home';
+import Cursos from './pages/Cursos';
+import Indicados from './pages/Indicados';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <Home />
+      <Router>
+          <Routes>
+              <Route path="/refugio-onirico" element={<Home/>}/>
+              <Route path="/refugio-onirico/cursos" element={<Cursos/>}/>
+              <Route path="/refugio-onirico/indicados" element={<Indicados/>}/>
+          </Routes>
+      </Router>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import logo from '../assets/img/logo.png';
-import menu from '../assets/icons/menu.png';
-import xsimbolo from '../assets/icons/simbolo-x.png';
+import menu from '../assets/icons/menu.webp';
+import xsimbolo from '../assets/icons/x.webp';
 
 const Header = () => {
   const [isMobileMenuOpen, setMobileMenuOpen] = useState(false);
@@ -16,24 +16,26 @@ const Header = () => {
     <nav className="bg-roxo z-10">
       <header className="">
         <div className="flex max-w-5xl mx-auto px-4 items-center">
-          <div className="flex-auto">
-            <a href="#"><img className="w-40 cursor-pointer" src={logo} alt="" /></a>
+        <div className="flex-auto">
+            <Link to="/refugio-onirico">
+              <img className="w-40 cursor-pointer" src={logo} alt="" />
+            </Link>
           </div>
 
           <div className="md:flex md:items-center md:ml-auto">
             <ul className={`hidden md:flex md:space-x-14 ${isMobileMenuOpen ? 'hidden' : 'block'}`}>
-              <li className={`${location.pathname === '/' ? 'text-amarelo font-bold' : 'text-white'}`}>
-                <Link to="/refugio-onirico" className="text-xl font-semibold">
+              <li className={`${location.pathname === '/cursos' ? 'text-amarelo font-bold' : 'text-white'}`}>
+                <Link to="/refugio-onirico/cursos" className="text-xl font-semibold">
                   Cursos
                 </Link>
               </li>
-              <li className={`${location.pathname === '/portfolio' ? 'text-amarelo font-bold' : 'text-white'}`}>
-                <Link to="/refugio-onirico" className="text-xl font-semibold">
+              <li className={`${location.pathname === '/indicados' ? 'text-amarelo font-bold' : 'text-white'}`}>
+                <Link to="/refugio-onirico/indicados" className="text-xl font-semibold">
                   E-books
                 </Link>
               </li>
-              <li className={`${location.pathname === '/portfolio' ? 'text-amarelo font-bold' : 'text-white'}`}>
-                <Link to="/refugio-onirico" className="text-xl text-white font-semibold">
+              <li className={`${location.pathname === '/indicados' ? 'text-amarelo font-bold' : 'text-white'}`}>
+                <Link to="/refugio-onirico/indicados" className="text-xl text-white font-semibold">
                   Cursos Indicados
                 </Link>
               </li>
@@ -54,17 +56,17 @@ const Header = () => {
 
           <ul className="flex flex-col text-center space-y-6">
             <li>
-              <Link to="/refugio-onirico" className={`text-2xl ${location.pathname === '/refugio-onirico' ? 'text-white font-bold' : 'text-amarelo'}`}>
+              <Link to="/refugio-onirico/cursos" className={`text-2xl ${location.pathname === '/refugio-onirico/cursos' ? 'text-white font-bold' : 'text-amarelo'}`}>
                 Cursos
               </Link>
             </li>
             <li>
-              <Link to="/refugio-onirico" className={`text-2xl ${location.pathname === '/refugio-onirico' ? 'text-white font-bold' : 'text-amarelo'}`}>
+              <Link to="/refugio-onirico/indicados" className={`text-2xl ${location.pathname === '/refugio-onirico/indicados' ? 'text-white font-bold' : 'text-amarelo'}`}>
                 Ebooks
               </Link>
             </li>
             <li>
-              <Link to="/refugio-onirico" className={`text-2xl ${location.pathname === '/refugio-onirico' ? 'text-white font-bold' : 'text-amarelo'}`}>
+              <Link to="/refugio-onirico/indicados" className={`text-2xl ${location.pathname === '/refugio-onirico/indicados' ? 'text-white font-bold' : 'text-amarelo'}`}>
                 Cursos Indicados
               </Link>
             </li>
